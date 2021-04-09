496e3b69c2a949f9fa6880b7783f2503ca9de6a7
Elena <gavrilowwa@gmail.com>
2021-04-09 12:17:20 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args){
+     long TicketPrice = 5370;
+     int BonusMile = 20;
+     long TotalBonus = TicketPrice/BonusMile;
+
+      System.out.println(TotalBonus);
+  }
+}
